25af2b386bc03755210f510d3e189789be99cf38
Anwar-3108 <syed.sahab9628@gmail.com>
2023-09-17 22:36:20 +0530

    done

---
+++ d-5/async.js
@@ -0,0 +1,18 @@
+const fs = require('fs') ;
+
+/*
+const data = fs.readFileSync('read.txt')
+console.log('thiss is data')
+console.log(data.toString()) ;
+*/
+
+
+
+/*
+fs.readFile('read.txt', (err, data)=>{
+    if(err)console.log(err)
+    console.log(data.toString());
+})
+console.log('after asyncronus') ;
+*/
+
---
+++ d-5/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+/*
+fs.writeFile('read.txt', 'hey brogrammers',(error)=>{if(error) console.log(error)}) ;
+*/
+
+
+
+
+/*
+fs.appendFile('read.txt', '  wassup ', (err)=>{if(err)console.log(err)   }    )
+*/
+
+
+
+ /*
+fs.readFile('read.txt' , (err , data)=>{
+    if(err)console.log(err)
+    console.log(data.toString()) ;
+})
+*/
+
+
