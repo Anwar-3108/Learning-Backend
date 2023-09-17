c52b1c746d6d940c1ec53574ee5599589724a394
Anwar-3108 <syed.sahab9628@gmail.com>
2023-09-17 23:28:32 +0530

    osmod

---
+++ OsModule/osmod.js
@@ -0,0 +1,34 @@
+const os = require('os') ;
+
+/*
+// To check the archichecture of Operating system
+console.log(os.arch()) ;
+*/
+
+
+/*
+const freeMemory = os.freemem() ;
+
+console.log(`${freeMemory}  bytes`)
+*/
+
+
+/*
+const tm= os.totalmem();
+console.log(tm)
+*/
+
+
+
+/*
+const hostName = os.hostname() ;
+
+console.log(hostName)
+*/
+
+
+// console.log(os.platform())
+
+console.log(os.type()) ;
+
+
